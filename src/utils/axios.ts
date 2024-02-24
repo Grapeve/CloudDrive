35a9f4132e6c775f2c5193a4ace923ff94ff1ee9
lyj <351675195@qq.com>
2024-02-24 16:39:07 +0800

    feat(utils): axios添加请求与响应拦截器

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,26 +1,44 @@
 import axios from 'axios'
-import { localGet } from './localStorageFn'
+import {localGet, localRemove} from './localStorageFn'
 
 const server = axios.create({
-  baseURL: '',
-  headers: {
-    'X-Requested-With': 'XMLHttpRequest',
-    token: localGet('token') || '',
-    post: {
-      'Content-Type': 'application/jsona'
+    baseURL: '',
+    headers: {
+        'X-Requested-With': 'XMLHttpRequest',
+        token: localGet('token') || '',
+        post: {
+            'Content-Type': 'application/jsona'
+        }
     }
-  }
 })
 
 // 请求拦截器，后续有需要再添加
-server.interceptors.request.use((res) => {
-  return res
-})
+server.interceptors.request.use(
+    res => {
+        if (localGet('token')) { //判断token是否存在
+            res.headers.token = localGet('token');  //将token设置成请求头
+        }
+        return res;
+    },
+    err => {
+        return Promise.reject(err);
+    }
+)
 
 // 响应拦截器，后续有需要再添加
-server.interceptors.response.use((res) => {
-  return res
-})
+server.interceptors.response.use(
+    response => {
+        if (response.data.code === 999) {
+            localRemove("token");
+            // router.replace('/');
+            console.log("token过期");
+        }
+        return response;
+    },
+    error => {
+        return Promise.reject(error);
+    }
+);
 
 export default server
 
